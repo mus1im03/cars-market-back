6a4e800ad2b6fcb5312ec8f80ebc8a6fd43237f3
Muslim Bariev <muslim@MacBook-Air-Muslim.local>
2023-11-28 22:46:13 +0300

    back

---
+++ index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const cors = require("cors");
+const morgan = require("morgan");
+const path = require('path')
+
+app.use('/public/img', express.static(path.resolve(__dirname, 'public', 'img')))
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+
+const { PORT, MONGO_SERVER } = process.env;
+
+connectAndStartServer = async () => {
+  try {
+    await mongoose.connect(MONGO_SERVER);
+    app.listen(PORT, () => {
+      console.log(`Сервер запущен: http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(`Ошибка при подключении: ${error.toString()}`);
+  }
+};
+
+connectAndStartServer();
