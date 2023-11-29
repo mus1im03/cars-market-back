4f3a0bf17d1f9afb6152a4e9f66ea1b97e8dea02
Halifat <hanif.001@mail.ru>
2023-11-29 16:34:51 +0300

    сделал машины

---
+++ routes/cars.route.js
@@ -0,0 +1,9 @@
+const { Router } = require("express");
+const {carscontroller} = require("../controllers/car.controller")
+ 
+const router = Router();
+
+router.get('/cars', carscontroller.getCars)
+router.post('/car', carscontroller.postCar)
+
+module.exports.router;
