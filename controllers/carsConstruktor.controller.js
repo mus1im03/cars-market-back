c31fdb02e9e592df10117c34747189bcdca2e85f
Muslim Bariev <muslim@MacBook-Air-Muslim.local>
2023-12-04 15:08:42 +0300

    adda

---
+++ controllers/carsConstruktor.controller.js
@@ -0,0 +1,29 @@
+const CarConstruktor = require("../models/CarConstruktor.model");
+
+module.exports.carConstruktorController = {
+  getCarsKonstruktor: async (req, res) => {
+    const konstruktor = await CarConstruktor.find();
+
+    res.json(konstruktor);
+  },
+
+  postCarKonstruktor: async (req, res) => {
+    const { name, mpower, description, engineType, img, titleImg, powerReserve } =
+      req.body;
+
+    try {
+      const konstruktor = await CarConstruktor.create({
+        name,
+        description,
+        engineType,
+        img,
+        titleImg,
+        mpower,
+        powerReserve,
+      });
+      await res.json(konstruktor);
+    } catch (e) {
+      return res.status(401).json(e.toString());
+    }
+  },
+};
