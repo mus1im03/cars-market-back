bb22a2e174a7cb719db5c729e9c39b5eb7e232aa
Muslim Bariev <muslim@MacBook-Air-Muslim.local>
2023-12-08 01:47:56 +0300

    category

--- controllers/car.controller.js
+++ controllers/car.controller.js
@@ -1,72 +1,72 @@
 const Car = require("../models/Car.model");
 
 module.exports.carsController = {
-    getCars: async (req, res) => {
-        const car = await Car.find();
-    
-        res.json(car);
-      },
+  getCars: async (req, res) => {
+    const car = await Car.find();
 
-      postCar: async (req, res) => {
-        const {
-            image,
-            name,
-            year,
-            price,
-            mileage,
-            owner,
-            transmission,
-            drive_unit,
-            working_volume,
-            maximum_torque,
-            maximum_power,
-            number_of_cylinders,
-            maximum_speed,
-            acceleration_time,
-            combined_cycle,
-            extra_urban_cycle,
-            urban_cycle,
-            co2_emissions,
-            length_mm,
-            height_mm,
-            width_mm,
-            ground_clearance,
-            unladen_weight,
-            permissible_total,
+    res.json(car);
+  },
 
+  postCar: async (req, res) => {
+    const {
+      categoryId,
+      image,
+      name,
+      year,
+      price,
+      mileage,
+      owner,
+      transmission,
+      drive_unit,
+      working_volume,
+      maximum_torque,
+      maximum_power,
+      number_of_cylinders,
+      maximum_speed,
+      acceleration_time,
+      combined_cycle,
+      extra_urban_cycle,
+      urban_cycle,
+      co2_emissions,
+      length_mm,
+      height_mm,
+      width_mm,
+      ground_clearance,
+      unladen_weight,
+      permissible_total,
+    } = req.body;
 
-        } = req.body;
-    
-        try {
-          const car = await Car.create({
-            image,
-            name,
-            year,
-            price,
-            mileage,
-            owner,
-            transmission,
-            drive_unit,
-            working_volume,
-            maximum_torque,
-            maximum_power,
-            number_of_cylinders,
-            maximum_speed,
-            acceleration_time,
-            combined_cycle,
-            extra_urban_cycle,
-            urban_cycle,
-            co2_emissions,
-            length_mm,
-            height_mm,
-            width_mm,
-            ground_clearance,
-            unladen_weight,
-            permissible_total,
-          });
-          await res.json(car);
-        } catch (e) {
-          return res.status(401).json(e.toString());
-        }
-      },
-}
+    try {
+      const car = await Car.create({
+        categoryId,
+        image,
+        name,
+        year,
+        price,
+        mileage,
+        owner,
+        transmission,
+        drive_unit,
+        working_volume,
+        maximum_torque,
+        maximum_power,
+        number_of_cylinders,
+        maximum_speed,
+        acceleration_time,
+        combined_cycle,
+        extra_urban_cycle,
+        urban_cycle,
+        co2_emissions,
+        length_mm,
+        height_mm,
+        width_mm,
+        ground_clearance,
+        unladen_weight,
+        permissible_total,
+      });
+      await res.json(car);
+    } catch (e) {
+      return res.status(401).json(e.toString());
+    }
+  },
+};
